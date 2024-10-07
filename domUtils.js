8242937c4374f989a844f923af35be898a9191e4
JadeMin <36400787+JadeMin@users.noreply.github.com>
2024-10-07 10:09:55 +0900

    refactor: more safety method

--- domUtils.js
+++ domUtils.js
@@ -19,17 +19,17 @@ class DOMTypeError extends Error {
 /**
  * @template T should be a subclass of HTMLElement
  * @param {string} selector 
- * @param {{ new (): T }} typeInstance 
+ * @param {{ new (): T }} instance 
  * @returns {T}
  */
-export function querySelector_s(selector, typeInstance) {
+export function querySelector_s(selector, instance) {
 	const element = document.querySelector(selector);
 	
 	if (element === null) {
 		throw new DOMQueryError(`Cannot find element by selector: "${selector}"`);
 	}
-	if (!(element instanceof typeInstance)) {
-		throw new DOMTypeError(`"${selector}": Expected <${typeInstance.name}>, but <${element.constructor.name}>`);
+	if (instance !== null && !(element instanceof instance)) {
+		throw new DOMTypeError(`"${selector}": Expected <${instance.name}>, but <${element.constructor.name}>`);
 	}
 
 	return element;
